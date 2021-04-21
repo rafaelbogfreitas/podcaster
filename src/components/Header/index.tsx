e1045dfd913c15cd14886cdd523032232411e049
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-04-21 10:20:22 -0300

    Finish header component and app setup

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import format from "date-fns/format";
+import ptBr from "date-fns/locale/pt-BR";
+import styles from "./style.module.scss";
+
+const Header = () => {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {locale: ptBr});
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="podcaster logo" />
+      <p>O melhor para você ouvir sempre</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+};
+
+export default Header;
