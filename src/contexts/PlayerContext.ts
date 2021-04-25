1a3db78c8b4c4fb9bde6f76af9308baab3805591
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-04-24 21:01:47 -0300

    Uses a ref to an audio element and implements play/pause actions

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -11,7 +11,10 @@ type Episode = {
 type PlayerContextData = {
     episodesList: Array<Episode>;
     currentEpisodeIndex: number;
+    isPlaying: boolean;
     play: (episode: Episode) => void;
+    togglePlay: () => void;
+    setPlayingState: (state: boolean) => void;
 };
 
 export const PlayerContext = createContext({} as PlayerContextData);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,25 @@ import { PlayerContext } from '../contexts/PlayerContext';
 function MyApp({ Component, pageProps }) {
     const [episodesList, setEpisodeList] = useState([]);
     const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     function play(episode) {
         setEpisodeList([episode]);
         setCurrentEpisodeIndex(0);
+        setIsPlaying(true);
+    }
+
+    function togglePlay() {
+        setIsPlaying(!isPlaying)
+    }
+    
+    function setPlayingState(state: boolean) {
+        setIsPlaying(state)
     }
     return (
-        <PlayerContext.Provider value={{ episodesList, currentEpisodeIndex, play }}>
+        <PlayerContext.Provider
+            value={{ episodesList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}
+        >
             <div className={styles.wrapper}>
                 <main>
                     <Header />
