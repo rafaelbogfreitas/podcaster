52a4c7c052972d4cde79f6133f95c4997c06a2e3
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-04-25 08:12:28 -0300

    Refactor PlayerContext component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react';
-import '../styles/global.scss';
-
-import styles from '../styles/app.module.scss';
 import Header from '../components/Header';
 import Player from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
+
+import styles from '../styles/app.module.scss';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-    const [episodesList, setEpisodeList] = useState([]);
-    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-    const [isPlaying, setIsPlaying] = useState(false);
-
-    function play(episode) {
-        setEpisodeList([episode]);
-        setCurrentEpisodeIndex(0);
-        setIsPlaying(true);
-    }
-
-    function togglePlay() {
-        setIsPlaying(!isPlaying)
-    }
-    
-    function setPlayingState(state: boolean) {
-        setIsPlaying(state)
-    }
     return (
-        <PlayerContext.Provider
-            value={{ episodesList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}
-        >
+        <PlayerContextProvider>
             <div className={styles.wrapper}>
                 <main>
                     <Header />
@@ -35,7 +15,7 @@ function MyApp({ Component, pageProps }) {
                 </main>
                 <Player />
             </div>
-        </PlayerContext.Provider>
+        </PlayerContextProvider>
     );
 }
 
